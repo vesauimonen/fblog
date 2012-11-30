df38bef24fd319014fbcb0df1e0856b11d4b75ba
Vesa Uimonen <vesa.uimonen@gmail.com>
2012-11-30 15:58:50 +0200

    added db dropping to init_db()

--- fblog/database.py
+++ fblog/database.py
@@ -11,8 +11,6 @@ Base.query = db_session.query_property()
 
 
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
     import fblog.models
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
