0043cdf6ffd77e98aa902167e02f6f82105ceb67
Vesa Uimonen <vesa.uimonen@gmail.com>
2012-12-02 21:17:40 +0200

    post delete and edit added

--- fblog/__init__.py
+++ fblog/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
-from flask.ext.login import (LoginManager, current_user, login_required,
-                            login_user, logout_user, UserMixin, AnonymousUser,
-                            confirm_login, fresh_login_required)
+import re
+from flask import Flask, request, render_template, redirect, url_for, flash, \
+    abort
+from flask.ext.login import LoginManager, current_user, login_required, \
+    login_user, logout_user, UserMixin, AnonymousUser, confirm_login, \
+    fresh_login_required
 from fblog.database import db_session
-from fblog.models import Post, User, Anonymous
-from sqlalchemy import desc
+from fblog.models import Post, User, Anonymous, Pagination, \
+    get_posts_for_page, count_all_posts
+from jinja2 import evalcontextfilter, Markup, escape
+
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -16,6 +20,7 @@ USERS = {
     1: User(USERNAME, 1, PASSWORD)
 }
 USER_NAMES = dict((u.name, u) for u in USERS.itervalues())
+PER_PAGE = 5
 
 app = Flask(__name__)
 
@@ -33,7 +38,6 @@ login_manager.refresh_view = 'reauth'
 def load_user(id):
     return USERS.get(int(id))
 
-
 login_manager.init_app(app)
 
 
@@ -42,38 +46,68 @@ def shutdown_session(exception=None):
     db_session.remove()
 
 
-@app.route('/')
-def display_posts():
-    posts = Post.query.order_by(desc(Post.published))
-    for post in posts:
-        if len(post.content) > 250:
-            post.content = post.content[:250] + '...'
-    return render_template('posts.html', posts=posts)
+@app.route('/', defaults={'page': 1})
+@app.route('/page/<int:page>')
+def display_posts(page):
+    count = count_all_posts()
+    print count
+    posts = get_posts_for_page(page, app.config['PER_PAGE'], count)
+    if not posts and page != 1:
+        abort(404)
+    pagination = Pagination(page, app.config['PER_PAGE'], count)
+    return render_template('posts.html',
+        pagination=pagination,
+        posts=posts
+        )
 
 
-@app.route('/posts/<int:id>')
+@app.route('/post/<int:id>')
 def display_post(id):
-    post = Post.query.filter(Post.id == id).one()
+    post = Post.query.get(id)
     return render_template('post.html', post=post)
 
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/add', defaults={'id': None}, methods=['GET', 'POST'])
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
-def add_post():
+def add_post(id):
+    edit = True if id != None else False
+    if edit:
+        post = Post.query.get(id)
+    else:
+        post = None
     if request.method == 'POST':
-        tags = request.form['tags'].replace(' ', '').split(',')
-
-        post = Post(request.form['title'], request.form['content'], [])
+        if not edit:
+            post = Post(request.form['title'], request.form['content'], [])
         if not (request.form['title'] and request.form['content']):
             flash('Both title and content need to be specified.')
-            return render_template('add_post.html', post=post)
+            return render_template('add_post.html', post=post, edit=edit)
         else:
-            db_session.add(post)
-            db_session.commit()
-            flash('New post submitted.')
+            if edit:
+                post.title = request.form['title']
+                post.content = request.form['content']
+                db_session.commit()
+                flash('Post edited.')
+            else:
+                db_session.add(post)
+                db_session.commit()
+                flash('New post submitted.')
             return redirect(url_for('display_posts'))
-    else:
-        return render_template('add_post.html')
+    return render_template('add_post.html', post=post, edit=edit)
+
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_post(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('delete_post.html', post=post)
+    if request.form['confirm'] == 'yes':
+        db_session.delete(post)
+        db_session.commit()
+        flash('Post deleted.')
+        return redirect(url_for('display_posts'))
+    return redirect(url_for('display_posts'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -100,3 +134,22 @@ def logout():
     logout_user()
     flash('Logged out.')
     return redirect(url_for('display_posts'))
+
+
+def url_for_other_page(page):
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
+
+_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
+
+
+@app.template_filter()
+@evalcontextfilter
+def nl2br(eval_ctx, value):
+    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n')
+        for p in _paragraph_re.split(escape(value)))
+    if eval_ctx.autoescape:
+        result = Markup(result)
+    return result
