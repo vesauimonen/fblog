2791845f0c81f5c678dcc6eb8815f2cca9e20120
Vesa Uimonen <vesa.uimonen@gmail.com>
2012-12-02 12:57:30 +0200

    routes and templates for single post and creating a post added

--- fblog/__init__.py
+++ fblog/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.login import (LoginManager, current_user, login_required,
                             confirm_login, fresh_login_required)
 from fblog.database import db_session
 from fblog.models import Post, User, Anonymous
+from sqlalchemy import desc
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -43,11 +44,36 @@ def shutdown_session(exception=None):
 
 @app.route('/')
 def display_posts():
-    posts = Post.query.all()
+    posts = Post.query.order_by(desc(Post.published))
     for post in posts:
         if len(post.content) > 250:
             post.content = post.content[:250] + '...'
     return render_template('posts.html', posts=posts)
+
+
+@app.route('/posts/<int:id>')
+def display_post(id):
+    post = Post.query.filter(Post.id == id).one()
+    return render_template('post.html', post=post)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+@login_required
+def add_post():
+    if request.method == 'POST':
+        tags = request.form['tags'].replace(' ', '').split(',')
+
+        post = Post(request.form['title'], request.form['content'], [])
+        if not (request.form['title'] and request.form['content']):
+            flash('Both title and content need to be specified.')
+            return render_template('add_post.html', post=post)
+        else:
+            db_session.add(post)
+            db_session.commit()
+            flash('New post submitted.')
+            return redirect(url_for('display_posts'))
+    else:
+        return render_template('add_post.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
